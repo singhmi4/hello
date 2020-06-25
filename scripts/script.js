cf9f020cc3dfe939c2ee67710e2574ed734b360d
Michael Singh <singh.michaelanthony@gmail.com>
2020-06-25 19:51:39 +0000

    option to specify country code in form input

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,38 @@
 $(document).ready(function(){
   	const userNameInput = $('#exampleInputUserName1');
 	const passWordInput = $('#exampleInputPassword1');
+	const countryCodeInput = $('#inputCountryCode');
 	const submitInput = $('#submit');
+	
 	const modalDiv = $('#exampleModal');
 	const greetingSpan = $('#greeting');
 	const userNameSpan = $('#username');
-	
-	const geoLocationEndpoint = 'https://freegeoip.app/json/';
-	
-	// Retrieve country Code
-	$.getJSON(geoLocationEndpoint)
-		.done(function(geoData) {
-			console.log(geoData.country_code);
-			let greetingEndpoint = `https://fourtonfish.com/hellosalut/?cc=${geoData.country_code}`;
 		
-		// Retrieve Greeting 			
-		$.getJSON(greetingEndpoint)
-			.done(function (greetingData) {
-				console.log(greetingData);
-				greetingSpan.text(greetingData.hello);
-			})
-			.fail(function() {
-				console.log( "greeting error" );
-		 	})
-		  	.always(function() {
-				console.log( "greeting complete" );
-		  	});
-		})
-		.fail(function() {
-			console.log( "geolocation error" );
-		})
-		.always(function() {
-			console.log( "geolocation complete" );
-		});		 
+	// Retrieve country Code
+	// $.getJSON(geoLocationEndpoint)
+	// 	.done(function(geoData) {
+	// 		console.log(geoData.country_code);
+	// 		let greetingEndpoint = `https://fourtonfish.com/hellosalut/?cc=${geoData.country_code}`;
+		
+	// 	// Retrieve Greeting 			
+	// 	$.getJSON(greetingEndpoint)
+	// 		.done(function (greetingData) {
+	// 			console.log(greetingData);
+	// 			greetingSpan.text(greetingData.hello);
+	// 		})
+	// 		.fail(function() {
+	// 			console.log( "greeting error" );
+	// 	 	})
+	// 	  	.always(function() {
+	// 			console.log( "greeting complete" );
+	// 	  	});
+	// 	})
+	// 	.fail(function() {
+	// 		console.log( "geolocation error" );
+	// 	})
+	// 	.always(function() {
+	// 		console.log( "geolocation complete" );
+	// 	});		 
 
 	
 	// console.log(countryCode)
@@ -40,6 +40,58 @@ $(document).ready(function(){
 	$('form').on('submit', function() {
 		if (userNameInput.val().length > 0 && passWordInput.val().length > 0) {
 			userNameSpan.text(userNameInput.val());
+			
+			
+			if (countryCodeInput.val().length == 2 ) {
+				
+				const greetingEndpoint = `https://fourtonfish.com/hellosalut/?cc=${countryCodeInput.val()}`;
+
+					// Retrieve Greeting 			
+					$.getJSON(greetingEndpoint)
+						.done(function (greetingData) {
+							console.log(greetingData);
+							greetingSpan.text(greetingData.hello);
+						})
+						.fail(function() {
+							console.log( "greeting error" );
+							greetingSpan.text("Hello");
+						})
+						.always(function() {
+							console.log( "greeting complete" );
+						});
+				
+				
+			} else {
+				const geoLocationEndpoint = 'https://freegeoip.app/json/';
+
+				// Retrieve country Code
+				$.getJSON(geoLocationEndpoint)
+					.done(function(geoData) {
+						console.log(geoData.country_code);
+						const greetingEndpoint = `https://fourtonfish.com/hellosalut/?cc=${geoData.country_code}`;
+
+					// Retrieve Greeting 			
+					$.getJSON(greetingEndpoint)
+						.done(function (greetingData) {
+							console.log(greetingData);
+							greetingSpan.text(greetingData.hello);
+						})
+						.fail(function() {
+							console.log( "greeting error" );
+							greetingSpan.text("Hello");
+						})
+						.always(function() {
+							console.log( "greeting complete" );
+						});
+					})
+					.fail(function() {
+						console.log( "geolocation error" );
+					})
+					.always(function() {
+						console.log( "geolocation complete" );
+					});
+			}
+			
 			modalDiv.modal('toggle');
 		} 
 	return false;
