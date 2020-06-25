b4927c19f916ad1e99a6d50fe8eaeb1a159a0479
Michael Singh <singh.michaelanthony@gmail.com>
2020-06-25 19:23:36 +0000

    native greeting js implemented

--- scripts/script.js
+++ scripts/script.js
@@ -3,10 +3,43 @@ $(document).ready(function(){
 	const passWordInput = $('#exampleInputPassword1');
 	const submitInput = $('#submit');
 	const modalDiv = $('#exampleModal');
+	const greetingSpan = $('#greeting');
+	const userNameSpan = $('#username');
+	
+	const geoLocationEndpoint = 'https://freegeoip.app/json/';
+	
+	// Retrieve country Code
+	$.getJSON(geoLocationEndpoint)
+		.done(function(geoData) {
+			console.log(geoData.country_code);
+			let greetingEndpoint = `https://fourtonfish.com/hellosalut/?cc=${geoData.country_code}`;
+		
+		// Retrieve Greeting 			
+		$.getJSON(greetingEndpoint)
+			.done(function (greetingData) {
+				console.log(greetingData);
+				greetingSpan.text(greetingData.hello);
+			})
+			.fail(function() {
+				console.log( "greeting error" );
+		 	})
+		  	.always(function() {
+				console.log( "greeting complete" );
+		  	});
+		})
+		.fail(function() {
+			console.log( "geolocation error" );
+		})
+		.always(function() {
+			console.log( "geolocation complete" );
+		});		 
+
+	
+	// console.log(countryCode)
 	
 	$('form').on('submit', function() {
-	// do your things
 		if (userNameInput.val().length > 0 && passWordInput.val().length > 0) {
+			userNameSpan.text(userNameInput.val());
 			modalDiv.modal('toggle');
 		} 
 	return false;
